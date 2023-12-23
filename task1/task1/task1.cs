e3c9e20d5708a8bad0ef2be720694431d8a95c4f
Valdish <65449305+Valdish@users.noreply.github.com>
2023-12-23 17:06:58 +0400

    Add files via upload

---
+++ task1/task1/task1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+class CircularArray
+{
+    static void Main()
+    {
+
+        int[] arg = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+        int n = arg[0];
+        int m = arg[1];
+
+        if (n <= 0 || m <= 0)
+        {
+            Console.WriteLine("Размер массива и длина обхода должны быть положительными числами.");
+            return;
+        }
+
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = i + 1;
+        }
+
+        int currentIndex = 0;
+        do
+        {
+            Console.Write(array[currentIndex]);
+            currentIndex = (currentIndex + m - 1) % array.Length;
+        } while (currentIndex != 0);
+
+
+        Console.ReadLine();
+    }
+
+}
